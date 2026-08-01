d500da263d2caba56716b7010bbeff9732398388
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-08-01 14:59:16 +0600

    create content page added

---
+++ src/Components/Dashboard/CreateContentForm/ContentForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContentForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ContentForm;
---
+++ src/Pages/DashboardPages/CreateContentPage/CreateContent.tsx
@@ -0,0 +1,37 @@
+import { IoChevronForward } from "react-icons/io5";
+
+const CreateContent = () => {
+  return (
+    <div>
+      {/* breadcramb title */}
+      <div>
+        {" "}
+        {/* bread */}
+        <nav className="flex" aria-label="Breadcrumb">
+          <ol className="flex items-center space-x-2 text-sm">
+            <li>
+              <a href="#" className="text-text-primary hover:text-gray-700">
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <IoChevronForward className="text-text-primary " />
+            </li>
+
+            <li>
+              <a href="#" className="text-text-primary  font-medium">
+                Create New Content
+              </a>
+            </li>
+          </ol>
+        </nav>
+        {/* title */}
+        <h1 className="text-2xl md:text-3xl lg:text-4xl lg:font-bold mt-3      text-text-primary inter ">
+          Create New Blogs & Content
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default CreateContent;
